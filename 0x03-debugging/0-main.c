1d7d96ad31dc9a262a334ed716a904cfd7ea3b06
kinsdayo <dayogabriel@icloud.com>
2022-09-16 10:24:22 -0700

    0-main.c

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,8 +1,7 @@
 #include "main.h"
-
 /**
 * main - tests function that prints if integer is positive or negative
-* Return: 0
+* Return:0
 */
 
 int main(void)
@@ -10,7 +9,7 @@ int main(void)
 int i;
 
 i = 0;
-positive_or_negative(i);
+positive_or_negative('i')
 
-Return (0);
+Return(0);
 }
